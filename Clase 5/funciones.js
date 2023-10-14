167c58d92e909346583cda4e0b35a5935fb2349c
Fernando Gorraiz <fgorraiz@hotmail.com>
2023-10-14 16:53:10 -0300

    Prueba modificaciones DOM

--- Clase 5/funciones.js
+++ Clase 5/funciones.js
@@ -10,3 +10,13 @@ function indiceAleatorio(rango)
 let indice = indiceAleatorio(nombres.length);
 
 console.log(nombres[indice]);
+
+let padre = document.getElementById("personas");
+let personas = ["juan","flor","fer"];
+
+for(const persona of personas)
+{
+    let li = document.createElement("li");
+    li.innerHTML = persona;
+    padre.appendChild(li);
+}
